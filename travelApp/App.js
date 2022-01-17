6cb2e0b07a1e628664980a6816ef338237055620
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2022-01-17 17:28:54 +0300

    initial commit

---
+++ travelApp/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator  } from '@react-navigation/stack';
+
+import {
+  OnBoarding
+} from "./app/screens";
+
+const Stack = createStackNavigator ();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="OnBoarding" component={OnBoarding} options={{ headerShown: false}}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
